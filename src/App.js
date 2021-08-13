52dec8d3378343f88acd51186b0be8457505fd50
rafael.alexander05 <rafael.alexander05@gmail.com>
2021-08-12 20:40:08 -0600

    Inicio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+function HolaMundo(nombre, edad){
+  var presentacion =<div>
+  <h2>{nombre}</h2>
+  <h2>Mi edad actual es {edad}</h2>
+  </div>
+
+  return presentacion;
+}
+
 function App() {
+  var nombre = 'Rafael Torres';
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hola Bienvenidos Alumnps DPS mi nombre es  {HolaMundo(nombre,35)}
         </p>
         <a
           className="App-link"
